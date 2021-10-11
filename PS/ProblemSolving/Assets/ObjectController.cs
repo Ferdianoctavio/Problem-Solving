07a6f02bc92be4acb7bae6521e976387821a0cc7
Ferdian Octavio <56250115+Ferdianoctavio@users.noreply.github.com>
2021-10-12 03:22:36 +0700

    Problem9

---
+++ PS/ProblemSolving/Assets/ObjectController.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class ObjectController : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            GameManager.Instance.AddScore(1);
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ PS/ProblemSolving/Assets/PlayerController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+
+    void Start()
+    {
+
+    }
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+    }
+}
